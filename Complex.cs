7cf22b5e3b178d8bc5ecf89f0ead8dc37d24df2a
Lenni2001 <lennartsturm@gmx.de>
2022-11-10 16:49:57 +0100

    Initial commit

---
+++ Complex.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RechnerKomplex
+{
+    class Complex
+    {
+        private double a, b; //des Standardtyps "double", auf die von "außen" kein Zugriff möglich ist
+        public Complex() //Wird kein Parameter angegeben, so sind Real- und Imaginärteil mit 0 vorzubelegen.
+        {
+            a = 0;
+            b = 0;
+        }
+
+        public Complex(double a_, double b_) //eine Initialisierung mit 2 "double"-Parametern soll möglich sein. a als Realteil und b als Imaginärteil.
+        {
+            a = a_;
+            b = b_;
+        }
+        public Complex(double a_) // Bei Angabe nur eines Parameters erhält der Realteil diesen Wert, der Imaginärteil erhält den Wert 0
+        {
+            a = a_;
+            b = 0;
+        }
+
+        public Complex Calc(Complex comp, char op) //add, sub, mul und div
+        {
+            Complex returncomplex = new Complex();
+            double newa;
+            double newb;
+
+            if (op == '+')
+            {
+                newa = comp.a + a;
+                newb = comp.b + b;
+                returncomplex.a = newa;
+                returncomplex.b = newb;
+            }
+            else if (op == '-')
+            {
+                newa = a - comp.a;
+                newb = b - comp.b;
+                returncomplex.a = newa;
+                returncomplex.b = newb;
+            }
+            else if (op == '*')
+            {
+                newa = a * comp.a - b * comp.b;
+                newb = a * comp.a + b * comp.b;
+                returncomplex.a = newa;
+                returncomplex.b = newb;
+            }
+            else if (op == '/')
+            {
+                newa = (a * comp.a + b * comp.b) / (Math.Pow(comp.a, 2) + Math.Pow(comp.b, 2));
+                newb = (b * comp.a - a * comp.b) / (Math.Pow(comp.a, 2) + Math.Pow(comp.b, 2));
+                returncomplex.a = newa;
+                returncomplex.b = newb;
+            }
+
+            return returncomplex;
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex returncomplex = new Complex();
+            returncomplex.a = c1.a + c2.a;
+            returncomplex.b = c1.b + c2.b;
+            return returncomplex;
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex returncomplex = new Complex();
+            returncomplex.a = c1.a - c2.a;
+            returncomplex.b = c1.b - c2.b;
+            return returncomplex;
+        }
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            Complex returncomplex = new Complex();
+            returncomplex.a = c1.a * c2.a - c1.b * c2.b;
+            returncomplex.b = c1.a * c2.a + c1.b * c2.b;
+            return returncomplex;
+        }
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            Complex returncomplex = new Complex();
+            returncomplex.a = (c1.a * c2.a + c1.b * c2.b) / (Math.Pow(c2.a, 2) + Math.Pow(c2.b, 2));
+            returncomplex.b = (c1.b * c2.a - c1.a * c2.b) / (Math.Pow(c2.a, 2) + Math.Pow(c2.b, 2));
+            return returncomplex;
+        }
+
+
+        public override string ToString() //eine Komplexe Zahl in der Form „? + j·?“ auszugeben.
+        {
+            return a + " + j · " + b;
+        }
+
+        //Zur Ausgabe von a und b verwende ich GetA() und GetB(). 
+        public double GetA()
+        {
+            return a;
+        }
+
+        public double GetB()
+        {
+            return b;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RechnerKomplex
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void berechnenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!FieldsEmpty())
+            {
+                //Felder sollten alle parse-fähig sein (siehe unten)
+                double a1 = double.Parse(realteil1TextBox.Text);
+                double b1 = double.Parse(imagteil1TextBox.Text);
+                double a2 = double.Parse(realteil2TextBox.Text);
+                double b2 = double.Parse(imagteil2TextBox.Text);
+
+                //Instanz einer komplexen Zahl;
+                Complex comp1 = new Complex(a1, b1);
+                Complex comp2 = new Complex(a2, b2);
+
+                //char operatorToUse = ' ';
+                switch (operatorComboBox.SelectedIndex) //get selected operator
+                {
+                    case 1:
+                        //operatorToUse = '+';
+                        SetAnswerComplex(comp1 + comp2);
+                        break;
+                    case 2:
+                        //operatorToUse = '-';
+                        SetAnswerComplex(comp1 - comp2);
+                        break;
+                    case 3:
+                        //operatorToUse = '*';
+                        SetAnswerComplex(comp1 * comp2);
+                        break;
+                    case 4:
+                        //operatorToUse = '/';
+                        SetAnswerComplex(comp1 / comp2);
+                        break;
+
+                }
+
+                /* //for using the Calc() method
+                 
+            if (operatorToUse != ' ')
+            {
+                Complex comp3 = comp1.Calc(comp2, operatorToUse); //calculates with char of operator to use
+                //MessageBox.Show((comp1 / comp2).ToString());
+
+                realteilOutTextBox.Text = comp3.GetA().ToString(); //displays a value in readonly textbox
+                imagteilOutTextBox.Text = comp3.GetB().ToString(); //displays b value in readonly textbox
+                complexOutAsStringLabel.Content = comp3.ToString(); //displays as string
+            }
+                */
+            }
+            else
+            {
+                MessageBox.Show("Bitte fülle alle Felder aus.", ":("); //error message
+            }
+        }
+
+        private bool FieldsEmpty() //not the cleanest solution...
+        {
+            if (realteil1TextBox.Text.Length == 0)
+            {
+                return true;
+            }
+            else if (realteil2TextBox.Text.Length == 0)
+            {
+                return true;
+            }
+            else if (imagteil1TextBox.Text.Length == 0)
+            {
+                return true;
+            }
+            else if (imagteil2TextBox.Text.Length == 0)
+            {
+                return true;
+            }
+            else if (operatorComboBox.SelectedIndex == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void SetAnswerComplex(Complex complex)
+        {
+            realteilOutTextBox.Text = complex.GetA().ToString(); //displays a value in readonly textbox
+            imagteilOutTextBox.Text = complex.GetB().ToString(); //displays b value in readonly textbox
+            complexOutAsStringLabel.Content = complex.ToString(); //displays as string
+        }
+
+        private void TextBox_OnlyNumberInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var getText = textBox.Text.Insert(textBox.SelectionStart, e.Text); //get the text from input
+
+            double val;
+            e.Handled = !double.TryParse(getText, out val); //check if text is a double :)
+        }
+    
+    }
+}
